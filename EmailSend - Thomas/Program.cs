62e07660c38493ba973892e5d99bbed12701c289
StevenSteveStevenson <61776913+WovenBarbeque@users.noreply.github.com>
2020-03-11 11:43:57 +1100

    My attempt

---
+++ EmailSend - Thomas/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailSend___Thomas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //edit
+        }
+    }
+}
